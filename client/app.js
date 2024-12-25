cc2efe388fca3b9a5fce929ec2f4cb1cee09ae21
sahil-sureka06 <sahilsureka06@gmail.com>
2024-12-25 23:59:16 +0530

    app.js

--- client/app.js
+++ client/app.js
@@ -26,7 +26,7 @@ function getBathValue() {
     var location = document.getElementById("uiLocations");
     var estPrice = document.getElementById("uiEstimatedPrice");
   
-    var url = "https://house-prediction-model-9.onrender.com/predict_home_price";
+    var url = "http://0.0.0.0:10000/predict_home_price";
   
     $.post(url, {
         total_sqft: parseFloat(sqft.value),
@@ -46,7 +46,7 @@ function getBathValue() {
   
 function onPageLoad() {
     console.log( "document loaded" );
-    var url = "https://house-prediction-model-9.onrender.com/get_location_names";
+    var url = "http://0.0.0.0:10000/get_location_names";
     $.get(url,function(data, status) {
         console.log("got response for get_location_names request");
         if(data) {
