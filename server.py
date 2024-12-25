c634d4d2d884fe6ea8cf75ad44d25eae16a9634d
sahil-sureka06 <sahilsureka06@gmail.com>
2024-12-25 20:54:44 +0530

    server.py

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 import util
 
@@ -29,4 +30,5 @@ def predict_home_price():
 if __name__ == "__main__":
     print("Starting Python Flask Server For Home Price Prediction...")
     util.load_saved_artifacts()
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
